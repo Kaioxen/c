9e660b715924f259aaa2aaa67a3cecd822de2a6d
Kaioxen <hermawanwildan78@gmail.com>
2025-12-22 07:53:12 +0000

    char

---
+++ 00-getting-started/char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	char *_x;
+	char *_z;
+	char *_y;
+	char *_w;
+	
+	
+	_x = "What Is Your Name?";
+	_z = "Whyy Do You Learn C Language?";
+	_y = "Do You Like Coffe?";
+    _w = "?";
+     printf("%s\n", _x);  //%s => strings
+     printf("%s\n", _z);
+     printf("%s\n", _y);
+     printf("%s", _w);
+	
+	return 0;
+}
