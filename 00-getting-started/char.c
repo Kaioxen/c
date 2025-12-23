7085c8c51d673305a17d1644638da5a939f24a22
Kaioxen <hermawanwildan78@gmail.com>
2025-12-23 07:00:03 +0000

    push

--- 00-getting-started/char.c
+++ 00-getting-started/char.c
@@ -7,22 +7,27 @@ int main()
 	char *_y;
 	char *_w;
     char *_grade;
-    char *_hello, *_my, *_name, *_is, *_wildan; 
-
+    char _hello[] = "Hello";
+    char _my[] = "my";
+    char _name = "name";
+    char _is = "is";
+    char _wildan[] = "Wildan Hermawan";
+    
+/*
     _hello = "Hello";
     _my = "my";
     _name = "name";
     _is = "is";
     _wildan = "Wildan";
-    
+  */  
     _grade = "A";
 	printf("%s\n", _grade);
 	_x = "What Is Your Name?";
 	_z = "Whyy Do You Learn C Language?";
 	_y = "Do You Like Coffe?";
     _w = "Do you know CR7?";
-    printf("%s\n %s\n %s\n %s\n ", _x, _z, _y, _w);  
+    printf("%s %s %s %s\n ", _x, _z, _y, _w);  
 
-    printf("%d\n %d\n %d\n %d\n %d\n ", *_hello, *_my, *_name, *_is, *_wildan);
+    printf("%s %s %s %s %s\n ", *_hello, *_my, *_name, *_is, *_wildan);
 	return 0;
 }
