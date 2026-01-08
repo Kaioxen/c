9046c550693bb4194c8bcd7d25fb8c672b4633d8
Kaioxen <hermawanwildan78@gmail.com>
2026-01-08 12:53:26 +0000

    feat: program minta maaf x5

--- 01-control-flow/for.c
+++ 01-control-flow/for.c
@@ -3,9 +3,11 @@
 
 int main()
 {
+ 
+ char _maaf[] = "maafin gua ";
 
 for (int i = 1; i <= 5; i++) {
-    printf("%d ", i);  // Output: 1 2 3 4 5
+    printf("%s%d\n", _maaf, i);  // Output maafin gua 1, maafin gua 2, dst...
 }
 return 0;
 
