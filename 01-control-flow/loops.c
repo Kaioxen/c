04ba69e8926a71d1f609e49bc50e793de390fe9b
Kaioxen <hermawanwildan78@gmail.com>
2026-01-08 12:32:32 +0000

    feat: program loop while sederhana

---
+++ 01-control-flow/loops.c
@@ -0,0 +1,14 @@
+// loop_sederhana.c
+#include <stdio.h>
+
+int main()
+{
+    int _counter = 1;
+    
+    while (_counter <= 5) {
+        printf("hitungan ke %d\n", _counter);
+        _counter++;
+    }
+    
+    return 0;
+}
